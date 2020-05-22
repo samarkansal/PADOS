bf0a2762ff3934f815e09b067189c185ff050dec
Samar Kansal <sameerkansal27@gmail.com>
2020-05-22 17:43:28 +0530

    Backend-Profile Model added

---
+++ models/Profile.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  places: {
+    type: [String],
+  },
+  rating: {
+    type: mongoose.Decimal128,
+  },
+  bio: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
