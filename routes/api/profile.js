e17c041336d893362f5be837d36187455c77bc5d
Samar Kansal <sameerkansal27@gmail.com>
2020-05-22 23:14:46 +0530

    Added profile route to add/update profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -18,6 +19,47 @@ router.get("/me", auth, async (req, res) => {
       return res.status(400).json({ msg: "There is no profile for this user" });
     }
 
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route      POST api/profile
+// @desc       Create or update profile
+// @access     Private
+router.post("/", auth, async (req, res) => {
+  const {
+    bio,
+    rating,
+    places,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin,
+  } = req.body;
+
+  //Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (bio) profileFields.bio = bio;
+  if (rating) profileFields.rating = rating;
+  // Build social object
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (instagram) profileFields.social.instagram = instagram;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+
+  try {
+    let profile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profileFields },
+      { new: true, upsert: true }
+    );
     res.json(profile);
   } catch (err) {
     console.error(err.message);
