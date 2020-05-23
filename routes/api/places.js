690e6820e73f5ee232dc96847b4487cbe83d8800
Samar Kansal <sameerkansal27@gmail.com>
2020-05-23 23:40:25 +0530

    Added route to GET details about places from a text query

---
+++ routes/api/places.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const request = require("request");
+const config = require("config");
+
+// @route      GET api/places/textQuery
+// @desc       Get places from googleMapApi based on text
+// @access     Private
+
+router.get("/textQuery/:place_name", (req, res) => {
+  try {
+    const options = {
+      uri: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${
+        req.params.place_name
+      }&key=${config.get("googlePlacesKey")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No place found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
