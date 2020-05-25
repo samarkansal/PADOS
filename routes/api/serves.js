282e421aae4294f64ea2b3581282d76037bd52ae
Samar Kansal <sameerkansal27@gmail.com>
2020-05-25 22:17:54 +0530

    Added route to GET/Delete serves by ID

--- routes/api/serves.js
+++ routes/api/serves.js
@@ -73,4 +73,65 @@ router.post(
   }
 );
 
+// @route   GET api/serves
+// @desc    Get all serves
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const serves = await Serve.find().sort({ date: -1 });
+    res.json(serves);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/serves/:id
+// @desc    Get serve by ID
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const serve = await Serve.findById(req.params.id);
+
+    if (!serve) {
+      return res.status(404).json({ msg: "Serve not found" });
+    }
+
+    res.json(serve);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Serve not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/serves/:id
+// @desc    Delete a serve
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const serve = await Serve.findById(req.params.id);
+    // check if serve exist
+    if (!serve) {
+      return res.status(404).json({ msg: "Serve not found" });
+    }
+    // Check user
+    if (serve.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await serve.remove();
+
+    res.json({ msg: "Serve Removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Serve Not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
