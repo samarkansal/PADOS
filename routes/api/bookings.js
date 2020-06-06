db14447b5360f3ffc708a8dae59f244fbdc9a552
Samar Kansal <sameerkansal27@gmail.com>
2020-06-06 13:37:10 +0530

    Added PUT transition route to change state of a Booking

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -60,6 +60,36 @@ router.post(
   }
 );
 
-router.put("/");
+//  @route  Put api/bookings/transition/:serve_id/:state
+//  @desc   Change the state of a booking to 'state'
+//  @access Serve Owner
+router.put("/transition/:booking_id/:state", auth, async (req, res) => {
+  try {
+    const booking_id = req.params.booking_id;
+    const booking = await Booking.findById(booking_id);
+    if (!booking) {
+      return res.status(404).json({ msg: "Booking Not Found" });
+    }
+    const serve = await Serve.findById(booking.serve);
+    if (!serve) {
+      return res.status(404).json({ msg: "Serve not valid" });
+    }
+    if (req.user.id !== serve.user.toString()) {
+      return res.status(404).json({ msg: "User not Authorized" });
+    }
+
+    booking.state = req.params.state;
+
+    await booking.save();
+
+    res.json(booking);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Booking Not Found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
