c11f2c9997dee2600a60d3b23f6b800fdf302391
samar.kansal <samar.kansal@servicenow.com>
2020-09-20 17:39:36 +0530

    Added Register route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
 import "./App.css";
 
 const App = () => (
@@ -9,6 +13,11 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path="/" component={Landing} />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+      <Footer />
     </Fragment>
   </Router>
 );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Fragment>
+      <section className="bgimg-4">
+        <div className="dark-overlay">
+          <div className="container">
+            <h1 className="large">Sign Up</h1>
+            <p className="lead">Create an Account</p>
+            <div style={{ "font-size": "44px" }}>
+              <i className="fa fa-user-plus"></i>
+            </div>
+            <form action="dashboard.html" className="form">
+              <div className="form-group">
+                <input type="text" placeholder="Name" required />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  title="This site uses Gravatar, so if you want a profile image,use a Gravatar email"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input type="tel" placeholder="Mobile" required />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  required
+                  minlength="6"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  required
+                  minlength="6"
+                />
+              </div>
+              <input type="submit" value="Register" className="btn" />
+            </form>
+            <p className="my-.5">
+              Already have an account? <a href="login.html">Sign In</a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Register;
