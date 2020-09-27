9532720b616b14206ca71448a3622ba084edf53a
samar.kansal <samar.kansal@servicenow.com>
2020-09-27 09:56:02 +0530

    Hooked register's form data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, mobile, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Password do not match");
+    } else {
+      console.log(formData);
+    }
+  };
   return (
     <Fragment>
       <section className="bgimg-4">
@@ -8,38 +29,61 @@ const Register = () => {
           <div className="container">
             <h1 className="large">Sign Up</h1>
             <p className="lead">Create an Account</p>
-            <div style={{ "font-size": "44px" }}>
+            <div style={{ fontSize: "44px" }}>
               <i className="fa fa-user-plus"></i>
             </div>
-            <form action="dashboard.html" className="form">
+            <form className="form" onSubmit={(e) => onSubmit(e)}>
               <div className="form-group">
-                <input type="text" placeholder="Name" required />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  required
+                  name="name"
+                  value={name}
+                  onChange={(e) => onChange(e)}
+                />
               </div>
               <div className="form-group">
                 <input
                   type="email"
                   placeholder="Email Address"
                   title="This site uses Gravatar, so if you want a profile image,use a Gravatar email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
                   required
                 />
               </div>
               <div className="form-group">
-                <input type="tel" placeholder="Mobile" required />
+                <input
+                  type="tel"
+                  placeholder="Mobile"
+                  name="mobile"
+                  value={mobile}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
               </div>
               <div className="form-group">
                 <input
                   type="password"
                   placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
                   required
-                  minlength="6"
+                  minLength="6"
                 />
               </div>
               <div className="form-group">
                 <input
                   type="password"
                   placeholder="Confirm Password"
+                  name="password2"
+                  value={password2}
+                  onChange={(e) => onChange(e)}
                   required
-                  minlength="6"
+                  minLength="6"
                 />
               </div>
               <input type="submit" value="Register" className="btn" />
